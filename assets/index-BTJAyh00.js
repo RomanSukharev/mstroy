4704cd435de2c3f30e79df41017863f88f53f41c
Roman Sukharev <romasukharev8@gmai.com>
2025-08-05 23:33:44 +0500

    second adding dist

--- assets/index-BTJAyh00.js
+++ assets/index-BTJAyh00.js
@@ -81,11 +81,17 @@ If you are trying to annotate ${s} with application data, use the '${s}.context'
           <div style="display:flex; justify-content:space-between; align-items:center;">
             <span>${a}</span>
             <div style="display:flex; gap:4px; align-items:center;">
-              <div class="tree-grid__btn tree-grid__btn--add">
-                <img src="/src/assets/icons/icon-plus.svg"/>
+              <div class="tree-grid__btn tree-grid__btn--add" title="Добавить">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <line x1="12" y1="5" x2="12" y2="19"></line>
+                  <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
               </div>
-              <div class="tree-grid__btn tree-grid__btn--remove">
-                <img src="/src/assets/icons/icon-plus.svg"/>
+              <div class="tree-grid__btn tree-grid__btn--remove" title="Удалить">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="transform: rotate(45deg);">
+                  <line x1="12" y1="5" x2="12" y2="19"></line>
+                  <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
               </div>
             </div>
           </div>
